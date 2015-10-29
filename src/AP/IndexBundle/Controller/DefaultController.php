5476dbd5520276c0c468c15dfa022b8862bea363
julienbouysset <julien.bouysset@hotmail.fr>
2015-10-29 19:32:40 +0100

    change bundle yml -> annotation

--- src/AP/IndexBundle/Controller/DefaultController.php
+++ src/AP/IndexBundle/Controller/DefaultController.php
@@ -3,11 +3,17 @@
 namespace AP\IndexBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/hello/{name}")
+     * @Template()
+     */
     public function indexAction($name)
     {
-        return $this->render('APIndexBundle:Default:index.html.twig', array('name' => $name));
+        return array('name' => $name);
     }
 }
